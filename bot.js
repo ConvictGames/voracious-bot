1f18ca656e1437128ad4b56866c6049fdbb6ba38
ConvictGames <38226129+ConvictGames@users.noreply.github.com>
2018-04-09 14:52:37 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,18 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+
+Client = discord.Client() #Initialise Client 
+client = commands.Bot(command_prefix = "?") #Initialise client bot
+
+@client.event
+async def on_ready():
+    print("Bot is online and connected to Discord") #This will be called when the bot connects to the server
+
+loop = asyncio.get_event_loop()
+@client.event
+async def on_message(message):
+        
+client.login(process.env.BOT_TOKEN);
