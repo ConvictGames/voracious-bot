8220e3d2c65f928e3dfcf82e8e29594ccfdfd57c
ConvictGames <38226129+ConvictGames@users.noreply.github.com>
2018-04-09 16:00:13 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,10 +5,10 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'Voracious W') {
-    	client.message.send('Voracious Wins!');
-  	}
+chatroom.on('message', function(msg){
+    if(msg.content === 'ping'){
+        msg.reply('pong');
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
