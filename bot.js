22d5443968f6880fdbe270ddd511b9fd5cc61525
ConvictGames <38226129+ConvictGames@users.noreply.github.com>
2018-04-09 15:26:38 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,43 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN):
-
-import discord
-from discord.ext.commands import Bot
-from discord.ext import commands
-import asyncio
-import time
-
-Client = discord.Client() #Initialise Client 
-client = commands.Bot(command_prefix = "?") #Initialise client bot
-
-@client.event
-async def on_ready():
-    print("Bot is online and connected to Discord") #This will be called when the bot connects to the server
-
-loop = asyncio.get_event_loop()
-@client.event
-async def on_message(message):
-    if 'Voracious W' in message.content:
-        await client.send_message(message.channel, "W")
-    if 'Voracious L' in message.content:
-        await client.send_message(message.channel, "L")
-    if 'Lacuna W' in message.content:
-        await client.send_message(message.channel, "W")
-    if 'Lacuna L' in message.content:
-        await client.send_message(message.channel, "L")
-    if 'TRIX W' in message.content:
-        await client.send_message(message.channel, "W")
-    if 'TRIX L' in message.content:
-        await client.send_message(message.channel, "L")
-    if 'Perihelion W' in message.content:
-        await client.send_message(message.channel, "W")
-    if 'Perihelion L' in message.content:
-        await client.send_message(message.channel, "L")        
-    if '3DT W' in message.content:
-        await client.send_message(message.channel, "W")
-    if '3DT L' in message.content:
-        await client.send_message(message.channel, "L")
-        
-loop.run_forever()
+client.login(process.env.BOT_TOKEN);
