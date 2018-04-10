451797341ed55be3c0357e5d424c839757bbce3f
ConvictGames <38226129+ConvictGames@users.noreply.github.com>
2018-04-10 15:40:04 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'Voracious L') {
+    	message.reply('Data Processed');
+  	}
+});
+
 
 client.login(process.env.BOT_TOKEN);
